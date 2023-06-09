8ee5d780df51456192c8a82e9bdcc345f2f703eb
ABlessingFromTheLord <94542984+ABlessingFromTheLord@users.noreply.github.com>
2023-06-09 14:09:15 +0200

    Modified Samwel branch

--- Assets/Samwel/RandomMovement.cs
+++ Assets/Samwel/RandomMovement.cs
@@ -10,10 +10,13 @@ public class RandomMovement : MonoBehaviour
     private float ChangeMovement = 0f;
     [SerializeField] Rigidbody2D Rigidbody;
     [SerializeField] Camera Cam;
-    private Vector2 xmin;
-    private Vector2 xmax;
+    [SerializeField] bool isBoss;
     private bool OutOfX = false;
+    private bool OutOfLowerY = false;
+    private bool OutOfUpperY = false;
     private Vector2 Middle;
+    private Vector2 LowerY;
+    private Vector2 UpperY;
 
     private void Start()
     {
@@ -21,13 +24,17 @@ public class RandomMovement : MonoBehaviour
         {
             Cam = GetComponent<Camera>();
         }
+
         // Generate initial random direction
         GenerateRandomDirection();
         Middle = Cam.ViewportToWorldPoint(new Vector2(0.5f, 0.5f));
+        LowerY = Cam.ViewportToWorldPoint(new Vector2(0.5f, 0.2f));
+        UpperY = Cam.ViewportToWorldPoint(new Vector2(0.5f, 0.8f));
     }
 
     private void Update()
     {
+        // Exceeding the left lower end of the x axis
         if(transform.position.x <= Middle.x)
         {
             GenerateRandomDirection();
@@ -36,6 +43,26 @@ public class RandomMovement : MonoBehaviour
             OutOfX = true;
             ElapsedTime = 0f;
 
+        }
+
+        // Exceeding the lower end of the y axis
+        if (transform.position.y <= LowerY.y)
+        {
+            GenerateRandomDirection();
+            Rigidbody.velocity = randomDirection;
+            ChangeMovement = Random.Range(0.7f, 2f);
+            OutOfLowerY = true;
+            ElapsedTime = 0f;
+        }
+
+        // Exceeding the upper end of the y axis
+        if (transform.position.y >= UpperY.y) 
+        {
+            GenerateRandomDirection();
+            Rigidbody.velocity = randomDirection;
+            ChangeMovement = Random.Range(0.7f, 2f);
+            OutOfUpperY = true;
+            ElapsedTime = 0f;
         }
 
         if (ElapsedTime >= ChangeMovement)
@@ -58,14 +85,34 @@ public class RandomMovement : MonoBehaviour
             // Generate a random direction vector
             randomDirection = new Vector2(Random.Range(2f, 1f), Random.Range(2f, 1f));
             randomDirection.Normalize();
-            randomDirection *= 10f; // You can adjust the magnitude of the random direction here
+            randomDirection *= 10f; // Adjusting the magnitude of the random direction
             OutOfX = false;
+            return;
+        }
+
+        if (OutOfLowerY) 
+        {
+            // Generate a random direction vector
+            randomDirection = new Vector2(Random.Range(1f, 2f), Random.Range(1f, 2f));
+            randomDirection.Normalize();
+            randomDirection *= 10f; // Adjusting the magnitude of the random direction
+            OutOfLowerY = false;
+            return;
+        }
+
+        if (OutOfUpperY) 
+        {
+            // Generate a random direction vector
+            randomDirection = new Vector2(Random.Range(1f, -2f), Random.Range(1f, -2f));
+            randomDirection.Normalize();
+            randomDirection *= 10f; // Adjusting the magnitude of the random direction
+            OutOfUpperY = false;
             return;
         }
 
         // Generate a random direction vector
         randomDirection = new Vector2(Random.Range(-1f, 1f), Random.Range(-2f, 1f));
         randomDirection.Normalize();
-        randomDirection *= 10f; // You can adjust the magnitude of the random direction here
+        randomDirection *= 10f; // // Adjusting the magnitude of the random direction
     }
 }
